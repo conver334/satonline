9263471d17fd609e61fca60bb533badc2322219e
conver <conver334@gmail.com>
2022-04-04 23:47:59 +0800

    add element

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-menu-item index="1">在线</el-menu-item>
+  <el-menu-item index="2">下载</el-menu-item>
+  </el-menu>
   <router-view/>
 </template>
-
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  components:{Navbar},
+  data() {
+    return {
+      activeIndex: '1'
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      if(keyPath[0]==1)this.$router.push({name:'home'})
+      else this.$router.push({name:'download'})
+    }
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,111 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="40%">
+        <el-button type="primary" @click="buttonchose=true"  round> 文字输入 </el-button>
+        <el-button type="primary" @click="buttonchose=false" round> 文档上传 </el-button>
+        <div v-if="buttonchose">
+          子句个数<el-input-number v-model="clausenum" :min="1" :max="10" @change="handleChange" />
+          变量个数<el-input-number v-model="valnum" :min="1" :max="10" @change="handleChange" />
+          <el-input
+            v-model="textarea"
+            :autosize="{ minRows: 2, maxRows: 10000000}"
+            type="textarea"
+            placeholder="input cnf"
+          />
+        </div>
+        <div v-else>
+          <el-upload
+            class="upload-demo"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :on-preview="handlePreview"
+            :on-remove="handleRemove"
+            :before-remove="beforeRemove"
+            multiple
+            :limit="3"
+            :on-exceed="handleExceed"
+            :file-list="fileList"
+          >
+            <el-button type="primary">Click to upload</el-button>
+            <template #tip>
+              <div class="el-upload__tip">
+                jpg/png files with a size less than 500KB.
+              </div>
+            </template>
+          </el-upload>
+        </div>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
   </div>
+
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { ElMessage, ElMessageBox } from 'element-plus'
+import type { UploadProps, UploadUserFile } from 'element-plus'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  // data(){
+  //   return {
+  //     buttonchose:true,
+  //   }
+  // },
+  // methods:{
+  // },
+  // components: {
+
+  // },
+  setup(){
+    const buttonchose = ref(true)
+    const clausenum = ref(1)
+    const valnum = ref(1)
+    const textarea = ref('')
+    const handleChange = (value) => {
+      console.log(value)
+    }
+    const fileList = ref<UploadUserFile[]>([
+      {
+        name: 'food.jpeg',
+        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+      },
+      {
+        name: 'food2.jpeg',
+        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+      },
+    ])
+
+    const handleRemove: UploadProps['onRemove'] = (file, uploadFiles) => {
+      console.log(file, uploadFiles)
+    }
+
+    const handlePreview: UploadProps['onPreview'] = (uploadFile) => {
+      console.log(uploadFile)
+    }
+
+    const handleExceed: UploadProps['onExceed'] = (files, uploadFiles) => {
+      ElMessage.warning(
+        `The limit is 3, you selected ${files.length} files this time, add up to ${
+          files.length + uploadFiles.length
+        } totally`
+      )
+    }
+
+    const beforeRemove: UploadProps['beforeRemove'] = (uploadFile, uploadFiles) => {
+      return ElMessageBox.confirm(
+        `Cancel the transfert of ${uploadFile.name} ?`
+      ).then(
+        () => true,
+        () => false
+      )
+    }
+    return {textarea,valnum,clausenum,handleChange,buttonchose,fileList,handleRemove,handlePreview,handleExceed,beforeRemove}
+  },
+  
 }
 </script>
+
+
+
