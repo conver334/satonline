3a7ec81e084294684d47824906e09a4d95a67b30
conver <conver334@gmail.com>
2022-04-05 10:15:05 +0800

    no upload

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,24 +15,7 @@
           />
         </div>
         <div v-else>
-          <el-upload
-            class="upload-demo"
-            action="https://jsonplaceholder.typicode.com/posts/"
-            :on-preview="handlePreview"
-            :on-remove="handleRemove"
-            :before-remove="beforeRemove"
-            multiple
-            :limit="3"
-            :on-exceed="handleExceed"
-            :file-list="fileList"
-          >
-            <el-button type="primary">Click to upload</el-button>
-            <template #tip>
-              <div class="el-upload__tip">
-                jpg/png files with a size less than 500KB.
-              </div>
-            </template>
-          </el-upload>
+         
         </div>
       </el-aside>
       <el-main>Main</el-main>
@@ -41,71 +24,21 @@
 
 </template>
 
-<script lang="ts" setup>
+<script >
 import { ref } from 'vue'
-import { ElMessage, ElMessageBox } from 'element-plus'
-import type { UploadProps, UploadUserFile } from 'element-plus'
-
-export default {
-  name: 'HomeView',
-  // data(){
-  //   return {
-  //     buttonchose:true,
-  //   }
-  // },
-  // methods:{
-  // },
-  // components: {
-
-  // },
-  setup(){
-    const buttonchose = ref(true)
-    const clausenum = ref(1)
-    const valnum = ref(1)
-    const textarea = ref('')
-    const handleChange = (value) => {
-      console.log(value)
-    }
-    const fileList = ref<UploadUserFile[]>([
-      {
-        name: 'food.jpeg',
-        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
-      },
-      {
-        name: 'food2.jpeg',
-        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
-      },
-    ])
-
-    const handleRemove: UploadProps['onRemove'] = (file, uploadFiles) => {
-      console.log(file, uploadFiles)
-    }
-
-    const handlePreview: UploadProps['onPreview'] = (uploadFile) => {
-      console.log(uploadFile)
-    }
-
-    const handleExceed: UploadProps['onExceed'] = (files, uploadFiles) => {
-      ElMessage.warning(
-        `The limit is 3, you selected ${files.length} files this time, add up to ${
-          files.length + uploadFiles.length
-        } totally`
-      )
-    }
-
-    const beforeRemove: UploadProps['beforeRemove'] = (uploadFile, uploadFiles) => {
-      return ElMessageBox.confirm(
-        `Cancel the transfert of ${uploadFile.name} ?`
-      ).then(
-        () => true,
-        () => false
-      )
-    }
-    return {textarea,valnum,clausenum,handleChange,buttonchose,fileList,handleRemove,handlePreview,handleExceed,beforeRemove}
-  },
-  
+export default{
+setup(){
+  const buttonchose = ref(true)
+  const clausenum = ref(1)
+  const valnum = ref(1)
+  const handleChange = ()=>{
+    console.log("change")
+  }
+  return {
+    buttonchose,clausenum,valnum,handleChange
+  }
 }
+}
+
+
 </script>
-
-
-
